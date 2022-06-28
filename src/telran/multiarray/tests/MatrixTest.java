dd554afe5271c92818f5b2375ec5f804f43ccedb
TimurMP <titimurmamyshev@gmail.commur>
2022-06-28 21:24:04 +0300

    Baseline

--- src/telran/multiarray/tests/MatrixTest.java
+++ src/telran/multiarray/tests/MatrixTest.java
@@ -1,4 +1,37 @@
-import static org.junit.jupiter.api.Assertions.*;
+package telran.multiarray.tests;
+
+import org.junit.jupiter.api.Test;
+import telran.multiarray.tools.Matrix;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class MatrixTest {
-  
+
+    int[][] matrix1 = { { 1, 2, 3 } }; // 1x3
+    int[][] matrix2 = { { 1 }, { 2 }, { 3 } };// 3x1
+
+    @Test
+    void testSum() {
+        assertEquals(6, Matrix.sum(matrix1));
+        assertEquals(6, Matrix.sum(matrix2));
+    }
+
+    @Test
+    void testTransp() {
+        assertArrayEquals(matrix2, Matrix.transp(matrix1));
+    }
+
+    @Test
+    void testMultiply() {
+        int[][] expected = { { 14 } };
+        assertArrayEquals(expected, Matrix.multiply(matrix1, matrix2));
+        int[][] arr1 = { { 1, 2, 3 }, { 4, 5, 6 } };
+        int[][] arr2 = { { 1, 4 }, { 2, 5 }, { 3, 6 } };
+        int[][] arrExpected = { { 14, 32 }, { 32, 77 } };
+        assertArrayEquals(arrExpected, Matrix.multiply(arr1, arr2));
+    }
+
+
+
 }
--- src/telran/multiarray/tools/Matrix.java
+++ src/telran/multiarray/tools/Matrix.java
@@ -1,2 +1,18 @@
-package telran.multiarray.tools;public class Matrix {
+package telran.multiarray.tools;
+
+public class Matrix {
+    public static int sum(int[][] matrix){
+
+        return 0;
+    }
+
+
+    public static int[][] transp(int [][] matrix){
+        return null;
+    }
+
+    public static int[][] multiply(int[][] matrix1, int[][] matrix2) {
+        // TODO
+        return null;
+    }
 }
